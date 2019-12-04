93d096d1fc500d4b9eba1d9634cefa392af3d59c
nlealluna <nleal.luna@gmail.com>
2019-12-04 14:27:09 -0300

    primeira versão

---
+++ teste1.py
@@ -0,0 +1,10 @@
+import random
+
+def func():
+    soma = 0
+    i = 1
+    for i in (10000):
+     numero = random.randint(0, 100)
+     soma = soma + numero
+     print(soma)
+     return soma/10000
